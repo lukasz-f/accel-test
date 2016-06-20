3633e3a8d2afb846de732dccc368282e7ff760a1
lukasz-f <lukaszf6@gmail.com>
2016-06-20 20:42:21 +0200

    fish images added

--- src/app.js
+++ src/app.js
@@ -16,35 +16,55 @@ Accel.config({
 var wind = new UI.Window({
   backgroundColor: 'black'
 });
-var rect = new UI.Rect({
-  size: new Vector2(10, 10),
-  backgroundColor: 'white'
+var fishImage = new UI.Image({
+  size: new Vector2(14, 7)
 });
-var rectVect = new Vector2();
-var rectPos = rect.position()
+var fishVector = new Vector2();
+var fishPosition = fishImage.position()
     .addSelf(wind.size())
-    .subSelf(rect.size())
+    .subSelf(fishImage.size())
     .multiplyScalar(0.5);
-rectPosition();
-wind.add(rect);
+fishUpdate();
+wind.add(fishImage);
 wind.show();
 
 wind.on('accelData', function(e) {
-  rectPosUpdate(e);
-  console.log('accelData in');
+  fishVectorUpdate(e);
+  console.log('accelData');
 });
 
-function rectPosUpdate(e) {
-  var verticalMove = e.accels[0].x/100;
-  var horizontalMove = -e.accels[0].y/100;
-  rectVect.set(verticalMove, horizontalMove);
+function fishVectorUpdate(e) {
+  var verticalMove = e.accels[0].x/300;
+  var horizontalMove = -e.accels[0].y/300;
+  fishVector.set(verticalMove, horizontalMove);
 }
 
-function rectPosition() {
-  rectPos.addSelf(rectVect);
-  rect.position(rectPos);
-  console.log('rectPosition in');
-  setTimeout(rectPosition, 50);
+function fishUpdate() {
+  fishPositionUpdate();
+  fishImageUpdate();
+  console.log('fishUpdate');
+  setTimeout(fishUpdate, 50);
+}
+
+function fishPositionUpdate() {
+  fishPosition.addSelf(fishVector);
+  checkFishPositionInsideScreen();
+  fishImage.position(fishPosition);
+}
+
+function checkFishPositionInsideScreen() {
+  if (fishPosition.x < 0) fishPosition.x = 0;
+  if (fishPosition.y < 0) fishPosition.y = 0;
+  if (fishPosition.x > wind.size().x) fishPosition.x = wind.size().x;
+  if (fishPosition.y > wind.size().y) fishPosition.y = wind.size().y;
+}
+
+function fishImageUpdate() {
+  if (fishVector.x > 0) {
+    fishImage.image('images/fish-right-14x7-1.png');
+  } else if (fishVector.x < 0) {
+    fishImage.image('images/fish-left-14x7-1.png');
+  }
 }
 
 /*
